74bf70cf7c327d0cc91c2d2f55bd68136d367c56
Shamrock <lifewalker@gmail.com>
2025-04-15 13:12:00 +0900

    Refactor floating window handling in rnum.nvim to improve line number restoration

--- lua/rnum/init.lua
+++ lua/rnum/init.lua
@@ -1,9 +1,16 @@
+-- filepath: /Users/shugo/.local/share/nvim/lazy/rnum.nvim/lua/rnum/init.lua
 -- rnum.nvim
 
 local M = {}
 
 local number_width = 4
 local insert_mode_behavior = "absolute"  -- default
+
+-- Helper function to check if the current window is a floating window
+local function is_floating_window()
+  local config = vim.api.nvim_win_get_config(0)
+  return config.relative ~= ""
+end
 
 function M.setup(opts)
   opts = opts or {}
@@ -18,6 +25,7 @@ function M.setup(opts)
 
   vim.api.nvim_create_autocmd("InsertEnter", {
     callback = function()
+      if is_floating_window() then return end
       if insert_mode_behavior == "absolute" then
         vim.opt.number = true
         vim.opt.relativenumber = false
@@ -32,13 +40,39 @@ function M.setup(opts)
 
   vim.api.nvim_create_autocmd("InsertLeave", {
     callback = function()
+      if is_floating_window() then return end
       vim.opt.number = true
       vim.opt.relativenumber = true
     end,
   })
+
+  -- Ensure line numbers are restored when leaving a floating window
+  vim.api.nvim_create_autocmd("WinLeave", {
+    callback = function()
+      if is_floating_window() then return end
+      vim.opt.number = true
+      vim.opt.relativenumber = true
+      vim.opt.statuscolumn = [[%!v:lua.require'rnum'.render()]]
+    end,
+  })
+
+  -- Ensure line numbers and statuscolumn are restored when entering a normal window
+  vim.api.nvim_create_autocmd("WinEnter", {
+    callback = function()
+      if is_floating_window() then return end
+      vim.opt.number = true
+      vim.opt.relativenumber = true
+      vim.opt.statuscolumn = [[%!v:lua.require'rnum'.render()]]
+    end,
+  })
 end
 
+-- Update the render function to skip rendering for floating windows
 function M.render()
+  if is_floating_window() then
+    return ""
+  end
+
   local relnum = vim.v.relnum
   local lnum = vim.v.lnum
   local is_current = relnum == 0
